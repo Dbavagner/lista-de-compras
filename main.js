421c346475b99d579a6584ef1403db7f277473be
Vagner Santos <dbavagner@gmail.com>
2025-07-06 19:36:13 -0300

    Incluir tratamento de alteração

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let listaDeItens = [] 
+let itemAEditar 
 
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
@@ -48,6 +49,7 @@ function mostrarItem() {
                     <span class="itens-comprados is-size-5">${elemento.valor}</span>
                 </div>
                 <div>
+                
                     <i class="fa-solid fa-trash is-clickable deletar"></i>
                 </div>
             </li>
@@ -58,9 +60,10 @@ function mostrarItem() {
             `   <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
                     <div>
                         <input type="checkbox" class="is-clickable" />
-                        <input type="text" class="is-size-5" value="">${elemento.valor}</input>
+                        <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled' : ''}></input>
                     </div>
                     <div>
+                        ${index === Number(itemAEditar) ? '<button onclick="salvarEdicao()">': ''} <i class="fa-regular fa-floppy-disk is-clickable"></i></button><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
                         <i class="fa-solid fa-trash is-clickable deletar"></i>
                     </div>
                 </li>
@@ -74,13 +77,37 @@ function mostrarItem() {
         
     inputsCheck.forEach( i => {
         i.addEventListener('click', (evento) => {
-            console.log('entrou' +  evento.target.checked)
             valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens[valorDoElemento].checar = evento.target.checked
             mostrarItem()
-            console.log ('valor do Item ' + valorDoElemento + ',status:' + evento.target.checked)
-            
+        })
+    })
+
+    const deletarObjetos = document.querySelectorAll('.deletar')
+
+    deletarObjetos.forEach( i => {
+        i.addEventListener('click', (evento) => {
+            itemSelectionado = evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
+        })
+    })
+
+    
+    const editarItens = document.querySelectorAll(".editar")
+
+    editarItens.forEach( i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
         })
     })
 }
 
+function salvarEdicao() {
+
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+    console.log ('alterção:' + itemEditado)
+    listaDeItens[itemAEditar].valor = itemEditado.value
+    itemSelectionado = -1
+    mostrarItem()
+}
